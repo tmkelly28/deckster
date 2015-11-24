41a4d49f87aac375760711c537aba01ebaa4126d
tmkelly28 <tmkelly28@gmail.com>
2015-11-23 21:06:54 -0500

    Updated logo and added vivus

---
+++ browser/js/common/directives/deckster-logo-small/deckster-logo-small.js
@@ -0,0 +1,6 @@
+app.directive('decksterLogoSmall', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/deckster-logo-small/deckster-logo-small.template.html'
+    };
+});
--- server/db/models/card.js
+++ server/db/models/card.js
@@ -34,7 +34,7 @@ var templates = [
         svg:'<svg id="svg1" width="339" height="469" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">' +
                 '<g id="group">' +
                     '<title>Layer 1</title>' +
-                    '<rect fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x="3" y="3" width="333" height="463" rx="10" ry="10" id="svg_1"/>' +
+                    '<rect fill="none" stroke="#363636" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x="3" y="3" width="333" height="463" rx="10" ry="10" id="svg_1"/>' +
                 '</g>' +
             '</svg>',
         templateOrigin: 'Blank'
@@ -44,8 +44,8 @@ var templates = [
         svg: '<svg id="svg1" width="339" height="469" xmlns="http://www.w3.org/2000/svg">' +
                 '<g id="group">' +
                     '<title>Layer 1</title>' +
-                    '<rect fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x="3" y="3" width="333" height="463" rx="10" ry="10" id="svg_1"/>' +
-                    '<rect fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x="34.5" y="39" width="270" height="270" rx="0" ry="0" id="svg_2"/>' +
+                    '<rect fill="none" stroke="#363636" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x="3" y="3" width="333" height="463" rx="10" ry="10" id="svg_1"/>' +
+                    '<rect fill="none" stroke="#363636" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x="34.5" y="39" width="270" height="270" rx="0" ry="0" id="svg_2"/>' +
                 '</g>' +
             '</svg>',
         templateOrigin: 'Blank With Frame'
