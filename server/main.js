81a65ff8447251ce8b8fb67dad781725e92fc1c2
Thomas Kelly <tmkelly28@gmail.com>
2016-03-01 12:30:51 -0500

    Saving

--- server/main.js
+++ server/main.js
@@ -15,7 +15,7 @@ var createApplication = function () {
 
 var startServer = function () {
 
-    var PORT = process.env.PORT || 8080;
+    var PORT = process.env.PORT || 3000;
 
     server.listen(PORT, function () {
         console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
